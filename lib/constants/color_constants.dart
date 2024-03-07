50bb0dc75ec2abc13792c18d9a3a4c0f4f7d5559
MohammadNikmard <mnikmard2004@gmail.com>
2024-03-07 09:36:03 +0330

    init project

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PrimaryColors {
+  static const darkColor = Color(0xff1F1D2B);
+  static const softColor = Color(0xff252836);
+  static const blueAccentColor = Color(0xff12CDD9);
+}
+
+class SecondaryColors {
+  static const greenColor = Color(0xff22B07D);
+  static const redColor = Color(0xffFF7256);
+  static const orangeColor = Color(0xffFF8700);
+}
+
+class TextColors {
+  static const blackText = Color(0xff171725);
+  static const whiteText = Color(0xffFFFFFF);
+  static const wihteGreyText = Color(0xffF1F1F5);
+  static const darkGreyText = Color(0xff696974);
+  static const greyText = Color(0xff92929D);
+  static const lineDark = Color(0xffEAEAEA);
+}
---
+++ lib/theme/main_theme.dart
@@ -0,0 +1,22 @@
+import 'package:cinemax/constants/color_constants.dart';
+import 'package:flutter/material.dart';
+
+ThemeData mainTheme = ThemeData(
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontFamily: "MR",
+      color: TextColors.whiteText,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 12,
+      fontFamily: "MM",
+      color: TextColors.whiteText,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 12,
+      fontFamily: "MSB",
+      color: TextColors.whiteText,
+    ),
+  ),
+);
