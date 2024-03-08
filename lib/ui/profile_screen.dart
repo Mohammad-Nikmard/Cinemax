370aefcaf43fe9a14a9067aefd312da17e1a1825
MohammadNikmard <mnikmard2004@gmail.com>
2024-03-08 10:34:59 +0330

    add profile screen

---
+++ lib/ui/profile_screen.dart
@@ -0,0 +1,415 @@
+import 'package:cinemax/constants/color_constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20, top: 20),
+                child: Center(
+                  child: Text(
+                    "Profile",
+                    style: TextStyle(
+                      fontFamily: "MSB",
+                      fontSize: 16,
+                      color: TextColors.whiteText,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.only(bottom: 25),
+              sliver: _ProfileChip(),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.only(bottom: 25),
+              sliver: _AccountChip(),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.only(bottom: 25),
+              sliver: _GeneralChip(),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.only(bottom: 40),
+              sliver: _MoreChip(),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.only(bottom: 50),
+              sliver: SliverToBoxAdapter(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 56,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        width: 1.3,
+                        color: PrimaryColors.blueAccentColor,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Log Out",
+                      style: TextStyle(
+                        color: PrimaryColors.blueAccentColor,
+                        fontSize: 16,
+                        fontFamily: "MSB",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OptionChip extends StatelessWidget {
+  const _OptionChip({required this.title, required this.image, this.color});
+  final String title;
+  final String image;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 30,
+          width: 30,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: PrimaryColors.softColor,
+          ),
+          child: Center(
+            child: Image.asset(
+              image,
+              color: color,
+              height: 20,
+              width: 20,
+            ),
+          ),
+        ),
+        const SizedBox(width: 15),
+        Text(
+          title,
+          style: const TextStyle(
+            fontFamily: "MM",
+            fontSize: 14,
+            color: TextColors.whiteText,
+          ),
+        ),
+        const Spacer(),
+        Image.asset(
+          'assets/images/icon_arrow_right.png',
+          color: PrimaryColors.blueAccentColor,
+        ),
+      ],
+    );
+  }
+}
+
+class _GeneralChip extends StatelessWidget {
+  const _GeneralChip();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 332,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.2,
+            color: const Color(0xff252836),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "General",
+                style: TextStyle(
+                  fontFamily: "MSB",
+                  fontSize: 18,
+                  color: TextColors.whiteText,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              _OptionChip(
+                title: "Notification",
+                image: "assets/images/icon_bell.png",
+                color: TextColors.greyText,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "Language",
+                image: "assets/images/icon_globe.png",
+                color: TextColors.greyText,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "Country",
+                image: "assets/images/icon_flag.png",
+                color: TextColors.greyText,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "Clear Cache",
+                image: "assets/images/icon_bin.png",
+                color: TextColors.greyText,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MoreChip extends StatelessWidget {
+  const _MoreChip();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 259,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.2,
+            color: const Color(0xff252836),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "More",
+                style: TextStyle(
+                  fontFamily: "MSB",
+                  fontSize: 18,
+                  color: TextColors.whiteText,
+                ),
+              ),
+              SizedBox(height: 15),
+              _OptionChip(
+                title: "Legal and Policies",
+                image: "assets/images/icon_shield.png",
+                color: TextColors.greyText,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "Help & Feedback",
+                image: "assets/images/icon_question.png",
+                color: TextColors.greyText,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "About Us",
+                image: "assets/images/icon_alert.png",
+                color: TextColors.greyText,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _AccountChip extends StatelessWidget {
+  const _AccountChip();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 188,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.2,
+            color: const Color(0xff252836),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Account",
+                style: TextStyle(
+                  fontFamily: "MSB",
+                  fontSize: 18,
+                  color: TextColors.whiteText,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              _OptionChip(
+                title: "Member",
+                image: "assets/images/icon_profile.png",
+                color: PrimaryColors.blueAccentColor,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1.3,
+                color: Color(0xff252836),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              _OptionChip(
+                title: "Change Password",
+                image: "assets/images/icon_lock.png",
+                color: TextColors.greyText,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ProfileChip extends StatelessWidget {
+  const _ProfileChip();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 86,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1.2,
+            color: const Color(0xff252836),
+          ),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                radius: 25,
+                backgroundColor: PrimaryColors.blueAccentColor,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Mohammad",
+                    style: TextStyle(
+                      fontFamily: "MSB",
+                      fontSize: 16,
+                      color: TextColors.whiteText,
+                    ),
+                  ),
+                  Text(
+                    "mnikmard1344@gmail.com",
+                    style: TextStyle(
+                      fontFamily: "MM",
+                      fontSize: 14,
+                      color: TextColors.greyText,
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Image.asset(
+                'assets/images/icon_edit_light.png',
+                color: PrimaryColors.blueAccentColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
